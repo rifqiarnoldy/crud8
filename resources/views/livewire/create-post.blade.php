06c011934b03de365e055bfc5bc082dc7b86f746
Rifqi Arnoldy <rifqiarnoldy@gmail.com>
2021-11-15 16:01:50 +0700

    Update create-post.blade.php

--- resources/views/livewire/create-post.blade.php
+++ resources/views/livewire/create-post.blade.php
@@ -2,6 +2,6 @@
     {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
     <textarea class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:shadow-outline" rows="3"></textarea>
     <div class="flex justify-end">
-        <button class="px-4 py-2 bg-blue-500 text-white hover:bg-blue-500 rounded-md">Post</button>
+        <button class="mt-2 px-4 py-2 bg-red-600 text-white hover:bg-indigo-600 rounded-md">Post</button>
     </div>
 </div>

1ab3aef17c8b5a4ae444367d45fd57829fe17675
Rifqi Arnoldy <rifqiarnoldy@gmail.com>
2021-11-15 16:12:03 +0700

    Create Livewire

--- app/Http/Livewire/CreatePost.php
+++ app/Http/Livewire/CreatePost.php
@@ -2,12 +2,27 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Post;
 use Livewire\Component;
+use Illuminate\Support\Facades\Auth;
 
 class CreatePost extends Component
 {
+    public $body;
     public function render()
     {
+
         return view('livewire.create-post');
+    }
+
+    public function createPost()
+    {
+        # code...
+        Post::create([
+            'user_id' => Auth::id(),
+            'body' => $this->body
+        ]);
+
+        $this->body = "";
     }
 }
--- resources/views/livewire/create-post.blade.php
+++ resources/views/livewire/create-post.blade.php
@@ -1,7 +1,7 @@
 <div class="py-4 px-4">
-    {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
-    <textarea class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:shadow-outline" rows="3"></textarea>
+    {{ $body }}
+    <textarea wire:model="body" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:shadow-outline" rows="3"></textarea>
     <div class="flex justify-end">
-        <button class="mt-2 px-4 py-2 bg-red-700 text-white hover:bg-red-600 rounded-md">Post</button>
+        <button wire:click="createPost" class="mt-2 px-4 py-2 bg-red-700 text-white hover:bg-red-600 rounded-md">Post</button>
     </div>
 </div>
